5631583890ed542ff7771a959ac15754c519dc61
akthyang <akthyang@gmail.com>
2023-05-08 23:50:37 -0400

    changed overflow and added css for error page

--- frontend/src/components/game.js
+++ frontend/src/components/game.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import "./game.css";
 
 function Game(props) {
     return (
--- frontend/src/error-page.js
+++ frontend/src/error-page.js
@@ -1,16 +1,23 @@
 import { useRouteError } from "react-router-dom";
+import Navbar from "./components/navbar";
+import "./error-page.css";
 
 export default function ErrorPage() {
     const error = useRouteError();
     console.error(error);
 
     return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
-        </div>
+        <>
+            <Navbar/>
+            <div id="error-page">
+                <div id = "message">
+                    <h1>Oops!</h1>
+                    <p>Sorry, an unexpected error has occurred.</p>
+                    <p>
+                        <i>{error.statusText || error.message}</i>
+                    </p>
+                </div>
+            </div>
+        </>
     );
 }
